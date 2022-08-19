440c12a5a626f73d08d0bd51881a4c7ec2eba898
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-08-19 18:53:48 +0100

    Add files via upload

---
+++ Bezao Tasks/Task Two/index.two.js
@@ -0,0 +1,26 @@
+/* As someone who just started learning Javascript, and has never deployed any application,
+or code that I've written, I can't say I know the exact answer, but having encountered the 
+theory at some point, I'll just take a wild gues at what is required of me :)) */
+
+
+if (user == 'admin') {
+
+    /* To add an item to the menu [create element|push] */
+    const addString = prompt('What item would you like to add to the menu?');
+    document.getElementById('').push(addString);
+
+    /* To remove an item from the menu [splice|replace child] */
+     document.getElementById('').replaceChild('');
+    /* or */
+
+
+    /* To display all food items on the menu */
+
+    const classNames = document.querySelectorAll(className);
+    for (const className of classNames) {
+         let myDisplay = document.getElementsByClassName(className).textContent();
+         alert('myDisplay');
+    }
+
+}
+
